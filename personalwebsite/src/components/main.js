cb56a066507597511f032f01d12c76ee5cf6ed5f
steinkchristensen <steinc95@gmail.com>
2019-04-01 20:08:06 -0600

    create page structure

---
+++ personalwebsite/src/components/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import LandingPage from './landingpage';
+import AboutMe from './aboutme';
+import Projects from './projects';
+import Resume from './resume';
+import Contact from './contact';
+import {Switch, Route} from 'react-router-dom';
+
+
+const Main =  () => (
+	<Switch>
+		<Route exact path='/' component={LandingPage} />
+		<Route path='/aboutme' component={AboutMe} />
+		<Route path='/projects' component={Projects} />
+		<Route path='/resume' component={Resume} />
+		<Route path='/contact' component={Contact} />
+	</Switch>
+)
+
+export default Main;
